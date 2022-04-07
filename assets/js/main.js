2e23238bc6aeaf657d751f8138b203b755d588ea
Rabindra <drarabin@gmail.com>
2022-04-07 15:29:47 +0545

    completed testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,7 +99,7 @@ modalCloses.forEach((modalClose) => {
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-var swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
     navigation: {
@@ -115,7 +115,22 @@ var swiper = new Swiper('.portfolio__container', {
 
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
 
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
